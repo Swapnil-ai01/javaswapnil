1dc64552f6ce07dcd462b75142798f044396508f
Swapnil-ai01 <swapnil011199@gmail.com>
2025-03-24 19:37:53 +0530

    second commit

--- Task6.java
+++ Task6.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
 public class Task6 {
+    public static void main(String[] args){
+        Scanner num = new Scanner(System.in);
+        System.out.print("Write ")
+    }
     
 }
